9ecbe80cc316c1ed9c6dff2cca6ddb6e17864fa4
abdulmossawer <mossawer786@gmail.com>
2024-08-28 19:55:17 +0530

    Favicon

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./utils/db.js";
+import path from "path"
 
 const app = express();
 
@@ -11,6 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 import dotenv from "dotenv";
 dotenv.config({});
+
+//For hosting
+const __dirname = path.resolve();
+console.log(__dirname);
+
 
 //for cors
 const corsOptions = {
@@ -32,6 +38,8 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRouter)
 app.use("/api/v1/job", jobRouter)
 app.use("/api/v1/application", applicationRouter)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
 
 
